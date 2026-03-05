5782d5c8867d57aab0589cbd026848eee9f0f1fc
Amardeep <amardeepverma113@gmail.com>
2026-03-05 12:32:35 +0100

    added execption handling

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,13 +1,17 @@
 import pandas
-
 nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_code = {word.letter: word.code for (ind, word) in nato_data.iterrows()}
 
-user_input = input("Enter a word: ").upper()
-# split_user_input = [n.upper() for n in user_input]
 
-# word_for_letter = [value for letter in split_user_input for key,
-#                    value in nato_code.items() if letter == key]
+def generate_phonotics():
+    user_input = input("Enter a word: ").upper()
+    try:
+        word_for_letter = [nato_code[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonotics()
+    else:
+        print(word_for_letter)
 
-word_for_letter = [nato_code[letter] for letter in user_input]
-print(word_for_letter)
+
+generate_phonotics()
